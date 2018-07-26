5ceaf990fb746e6aa1890fd5e247bf66dfda56f4
ymsang <ymsang@mo9.com>
2018-07-26 20:43:41 +0800

    修改promise版为自动生成翻译稿

--- read_line_promise.js
+++ read_line_promise.js
@@ -19,45 +19,69 @@ class Splitter {
     this.outputDir = outputDir
     this.outputExt = outputExt
 
-    this.formatLine = this.formatLine.bind(this)
+    // this.formatLine = this.formatLine.bind(this)
     this.saveToFile = this.saveToFile.bind(this)
     this.getOutputFileName = this.getOutputFileName.bind(this)
     this.dataToStr = this.dataToStr.bind(this)
+
+    this.formatData = this.formatData.bind(this)
+    this.getLineArray = this.getLineArray.bind(this)
+
   }
 
-  go() {
-    fileUtil.readFile(this.dataFileName, 'utf-8')
-      .then(this.formatLine)
-      .then(this.saveToFile)
-      .then(() => {
-        log('已存入文件')
-      })
-      .catch(err => {
-        throw err
-      })
-  }
 
-  formatLine(data) {
-    let arr = data
-      .replace(/\r/g, '')
-      .replace(/^\s+||\s+$/g, '')
-      .split(/\n{2,}/g)
-    let result = []
-    for (let i = 0; i < arr.length; i += 3) {
-      result.push({
-        name: arr[i],
-        title: arr[i + 1],
-        avator: 1,
-        link: 2,
-        infoList: arr[i + 2] ? arr[i + 2].split(/\n{1,}/) : null,
-      })
+  // formatLine(data) {
+  //   let cleanData = data
+  //     .replace(/\r/g, '')
+  //     .replace(/^\s+||\s+$/g, '')
+  //   let arr = cleanData.split(/\n{2,}/g)
+
+  //   let result = []
+  //   for (let i = 0; i < arr.length; i += 3) {
+  //     result.push({
+  //       name: arr[i],
+  //       title: arr[i + 1],
+  //       avator: 1,
+  //       link: 2,
+  //       infoList: arr[i + 2] ? arr[i + 2].split(/\n{1,}/) : null,
+  //     })
+  //   }
+  //   return result
+  // }
+
+  formatData(data) {
+    let result = {}
+    let lines = this.getLineArray(data)
+    for (let line of lines) {
+      let parts = line.split(/\t/g)
+      let [id, cn, en] = parts
+      let languageStr = en
+      if (languageStr) {
+        let reg = /{.+?}/g
+        let match = languageStr.match(reg)
+        if (match) {
+          let formattedStr = languageStr.replace(reg, '{value}')
+          result[id] = formattedStr
+          if (match.length > 1) {
+            log(id, languageStr)
+          }
+        } else {
+          result[id] = languageStr
+        }
+      } else {
+        // log(JSON.stringify(parts, null, 2))
+      }
+
     }
     return result
-    // let p = new Promise((resolve, reject) => {
-    //   resolve(result)
-    // })
-    // return p
-    // return team
+  }
+
+  getLineArray(data) {
+    let cleanData = data
+      .replace(/\r/g, '')
+      .replace(/^\s+||\s+$/g, '')
+    let lines = cleanData.split(/\n{1,}/g)
+    return lines;
   }
 
   dataToStr(data) {
@@ -89,8 +113,23 @@ class Splitter {
     let filename = this.getOutputFileName()
     let dataStr = this.dataToStr(data)
     fileUtil.writeFile(filename, dataStr)
+    return filename;
+  }
+
+  go() {
+    fileUtil.readFile(this.dataFileName, 'utf-8')
+      .then(this.formatData)
+      .then(this.saveToFile)
+      .then((name) => {
+        log('已存入文件' + name)
+      })
+      .catch(err => {
+        throw err
+      })
   }
 }
 
-let s = new Splitter('data.txt')
+// let s = new Splitter('data.txt')
+let s = new Splitter('translateData.txt')
+
 s.go()
